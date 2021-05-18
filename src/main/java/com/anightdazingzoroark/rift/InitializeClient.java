20640ed38d6ebee212e7c6bced895941ee2a32fc
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2021-05-18 22:48:24 +0800

    what am i even doing

--- src/main/java/com/anightdazingzoroark/rift/InitializeClient.java
+++ src/main/java/com/anightdazingzoroark/rift/InitializeClient.java
@@ -1,10 +1,11 @@
 package com.anightdazingzoroark.rift;
 
+import com.anightdazingzoroark.rift.registry.ModRenderers;
 import net.fabricmc.api.ClientModInitializer;
 
 public class InitializeClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        ModRenderers.registerRenderers();
     }
 }
---
+++ src/main/java/com/anightdazingzoroark/rift/registry/ModRenderers.java
@@ -0,0 +1,12 @@
+package com.anightdazingzoroark.rift.registry;
+
+import com.anightdazingzoroark.rift.client.renderer.TyrannosaurusRenderer;
+import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
+
+public class ModRenderers {
+    public static void registerRenderers() {
+        EntityRendererRegistry.INSTANCE.register(ModEntities.TYRANNOSAURUS, (dispatcher, context) -> {
+            return new TyrannosaurusRenderer(dispatcher);
+        });
+    }
+}
