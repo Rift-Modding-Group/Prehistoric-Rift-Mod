6e55cd919d3e3b0d0cbb7a5eb3ebc82de0b9be58
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2021-05-20 14:51:34 +0800

    a

--- src/main/java/com/anightdazingzoroark/rift/entities/TyrannosaurusEntity.java
+++ src/main/java/com/anightdazingzoroark/rift/entities/TyrannosaurusEntity.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.mob.*;
 import net.minecraft.entity.passive.*;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.*;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
@@ -21,17 +22,37 @@ public class TyrannosaurusEntity extends TameableEntity implements IAnimatable, 
         super(entityType, world);
     }
 
-    protected void initGoals() {
-        this.goalSelector.add(2, new MeleeAttackGoal(this, 1, true));
-        this.goalSelector.add(3, new WanderAroundFarGoal(this, 1.0D));
-    }
-
-
     public static DefaultAttributeContainer.Builder createAttributes() {
         return MobEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 160)
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 35);
+    }
+
+    protected void initGoals() {
+        this.goalSelector.add(3, new MeleeAttackGoal(this, 1, true));
+        this.goalSelector.add(4, new WanderAroundFarGoal(this, 1.0D));
+        this.findTargets();
+    }
+
+    private void findTargets() {
+        this.targetSelector.add(2, new FollowTargetGoal(this, PlayerEntity.class, true));
+        this.targetSelector.add(2, new FollowTargetGoal(this, VillagerEntity.class, true));
+        this.targetSelector.add(2, new FollowTargetGoal(this, WanderingTraderEntity.class, true));
+        this.targetSelector.add(2, new FollowTargetGoal(this, PigEntity.class, true));
+        this.targetSelector.add(2, new FollowTargetGoal(this, SheepEntity.class, true));
+        this.targetSelector.add(2, new FollowTargetGoal(this, CowEntity.class, true));
+        this.targetSelector.add(2, new FollowTargetGoal(this, ChickenEntity.class, true));
+        this.targetSelector.add(2, new FollowTargetGoal(this, WolfEntity.class, true));
+        this.targetSelector.add(2, new FollowTargetGoal(this, CatEntity.class, true));
+        this.targetSelector.add(2, new FollowTargetGoal(this, OcelotEntity.class, true));
+        this.targetSelector.add(2, new FollowTargetGoal(this, FoxEntity.class, true));
+        this.targetSelector.add(2, new FollowTargetGoal(this, PolarBearEntity.class, true));
+        this.targetSelector.add(2, new FollowTargetGoal(this, HorseEntity.class, true));
+        this.targetSelector.add(2, new FollowTargetGoal(this, DonkeyEntity.class, true));
+        this.targetSelector.add(2, new FollowTargetGoal(this, MuleEntity.class, true));
+        this.targetSelector.add(2, new FollowTargetGoal(this, ParrotEntity.class, true));
+        this.targetSelector.add(2, new FollowTargetGoal(this, PandaEntity.class, true));
     }
 
     @Nullable
