16e169b015d89780c54543be5b7c6365d4255ab9
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2022-08-05 22:32:19 +0800

    moving to forge

---
+++ src/main/java/com/anightdazingzoroark/rift/RiftInitialize.java
@@ -0,0 +1,34 @@
+package com.anightdazingzoroark.rift;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(RiftInitialize.MODID)
+public class RiftInitialize {
+    public static final String MODID = "rift";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public RiftInitialize() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
