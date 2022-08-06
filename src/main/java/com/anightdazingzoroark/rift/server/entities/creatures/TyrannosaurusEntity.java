d85be8f0f48c803255846cb4d85cbdde5897348e
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2022-08-06 14:53:46 +0800

    more splash texts + tyranno behaviors

--- src/main/java/com/anightdazingzoroark/rift/server/entities/creatures/TyrannosaurusEntity.java
+++ src/main/java/com/anightdazingzoroark/rift/server/entities/creatures/TyrannosaurusEntity.java
@@ -6,6 +6,10 @@ import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.IAnimationTickable;
@@ -17,6 +21,13 @@ public class TyrannosaurusEntity extends RiftCreature implements IAnimatable, IA
 
     public TyrannosaurusEntity(EntityType<? extends TamableAnimal> type, Level world) {
         super(type, world);
+    }
+
+    protected void registerGoals() {
+        this.goalSelector.addGoal(0, new FloatGoal(this));
+        this.goalSelector.addGoal(6, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+        this.goalSelector.addGoal(7, new LookAtPlayerGoal(this, Player.class, 6.0F));
+        this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
     }
 
     public static AttributeSupplier.Builder createAttributes() {
