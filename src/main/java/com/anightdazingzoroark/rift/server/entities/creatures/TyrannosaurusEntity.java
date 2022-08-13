27745173fb4bc4b30ba4a9a97b82ced07cb6dc11
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2022-08-14 00:16:05 +0800

    pretty good progress today

--- src/main/java/com/anightdazingzoroark/rift/server/entities/creatures/TyrannosaurusEntity.java
+++ src/main/java/com/anightdazingzoroark/rift/server/entities/creatures/TyrannosaurusEntity.java
@@ -5,23 +5,24 @@ import com.anightdazingzoroark.rift.server.entities.RiftCreature;
 import com.anightdazingzoroark.rift.server.entities.goals.RiftAttackAnimalsGoal;
 import com.anightdazingzoroark.rift.server.entities.goals.RiftAttackGoal;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
-import net.minecraft.world.entity.animal.Pig;
-import net.minecraft.world.entity.monster.AbstractIllager;
-import net.minecraft.world.entity.monster.Ravager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
@@ -35,26 +36,34 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 import java.util.Objects;
 import java.util.Random;
 import java.util.function.Predicate;
-import java.util.random.RandomGenerator;
 
 public class TyrannosaurusEntity extends RiftCreature implements IAnimatable {
     private static final Predicate<Entity> ROAR_TARGETS = (entity) -> {
         return entity.isAlive() && !(entity instanceof TyrannosaurusEntity);
     };
     public static final EntityDataAccessor<Boolean> ROARING = SynchedEntityData.defineId(TyrannosaurusEntity.class, EntityDataSerializers.BOOLEAN);
+    public static final EntityDataAccessor<Boolean> HUNTING = SynchedEntityData.defineId(TyrannosaurusEntity.class, EntityDataSerializers.BOOLEAN);
+    public static final EntityDataAccessor<Integer> HUNTINGTICK = SynchedEntityData.defineId(TyrannosaurusEntity.class, EntityDataSerializers.INT);
     private final RiftAttackGoal attackGoal = new RiftAttackGoal(this, 0.5, 0.5, 1, true);
+    private final NearestAttackableTargetGoal attackPlayerGoal = new NearestAttackableTargetGoal<>(this, Player.class, true);
+    private final RiftAttackAnimalsGoal attackAnimalsGoal = new RiftAttackAnimalsGoal(this, true, true);
     private final AnimationFactory factory = new AnimationFactory(this);
 
     public TyrannosaurusEntity(EntityType<? extends TamableAnimal> type, Level world) {
         super(type, world);
     }
 
+    @Override
+    public SpawnGroupData finalizeSpawn(ServerLevelAccessor serverLevelAccessor, DifficultyInstance difficultyInstance, MobSpawnType mobSpawnType, @Nullable SpawnGroupData spawnGroupData, @Nullable CompoundTag compoundTag) {
+        this.setHunting(new Random().nextBoolean());
+        this.setHuntingTick(new Random().nextInt(900, 1500));
+        return super.finalizeSpawn(serverLevelAccessor, difficultyInstance, mobSpawnType, spawnGroupData, compoundTag);
+    }
+
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
         this.goalSelector.addGoal(1, new TyrannosaurusRoarGoal(this));
         this.targetSelector.addGoal(3, new HurtByTargetGoal(this));
-        this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, Player.class, true));
-        this.targetSelector.addGoal(4, new RiftAttackAnimalsGoal(this, true, true));
         this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0D));
         this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 6.0F));
         this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
@@ -72,6 +81,7 @@ public class TyrannosaurusEntity extends RiftCreature implements IAnimatable {
     @Override
     public void tick() {
         super.tick();
+        //for changing attributes when roaring
         if (!this.isRoaring()) {
             this.goalSelector.addGoal(2, this.attackGoal);
             Objects.requireNonNull(this.getAttribute(Attributes.MOVEMENT_SPEED)).setBaseValue(0.25D);
@@ -80,6 +90,34 @@ public class TyrannosaurusEntity extends RiftCreature implements IAnimatable {
             this.goalSelector.removeGoal(this.attackGoal);
             Objects.requireNonNull(this.getAttribute(Attributes.MOVEMENT_SPEED)).setBaseValue(0D);
         }
+
+        //for casting weakness
+        for(LivingEntity livingentity : this.level.getEntitiesOfClass(LivingEntity.class, this.getBoundingBox().inflate(12.0D), ROAR_TARGETS)) {
+            if (!livingentity.hasEffect(MobEffects.WEAKNESS) || livingentity.getEffect(MobEffects.WEAKNESS).getAmplifier() <= 1) {
+                livingentity.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 600, 1));
+            }
+        }
+
+        //for managing hunting system
+        if (this.isHunting()) {
+            this.goalSelector.addGoal(4, this.attackPlayerGoal);
+            this.goalSelector.addGoal(4, this.attackAnimalsGoal);
+        }
+        else {
+            this.goalSelector.removeGoal(this.attackPlayerGoal);
+            this.goalSelector.removeGoal(this.attackAnimalsGoal);
+        }
+
+        if (this.getHuntingTick() <= 0 && this.isHunting()) {
+            this.setHunting(false);
+            this.setHuntingTick(new Random().nextInt(900, 1500));
+        }
+        else if (this.getHuntingTick() <= 0 && !this.isHunting()) {
+            this.setHunting(true);
+            this.setHuntingTick(new Random().nextInt(900, 1500));
+        }
+
+        this.setHuntingTick(this.getHuntingTick() - 1);
     }
 
     private <E extends IAnimatable> PlayState movement(AnimationEvent<E> event) {
@@ -144,9 +182,40 @@ public class TyrannosaurusEntity extends RiftCreature implements IAnimatable {
         this.entityData.set(ROARING, bool);
     }
 
+    public boolean isHunting() {
+        return this.entityData.get(HUNTING);
+    }
+
+    public void setHunting(boolean bool) {
+        this.entityData.set(HUNTING, bool);
+    }
+    public int getHuntingTick() {
+        return this.entityData.get(HUNTINGTICK);
+    }
+
+    public void setHuntingTick(int huntingTick) {
+        this.entityData.set(HUNTINGTICK, huntingTick);
+    }
+
     protected void defineSynchedData() {
         super.defineSynchedData();
         this.entityData.define(ROARING, false);
+        this.entityData.define(HUNTING, false);
+        this.entityData.define(HUNTINGTICK, 0);
+    }
+
+    @Override
+    public void addAdditionalSaveData(CompoundTag compoundTag) {
+        super.addAdditionalSaveData(compoundTag);
+        compoundTag.putBoolean("IsHunting", this.isHunting());
+        compoundTag.putInt("HuntingTick", this.getHuntingTick());
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundTag compoundTag) {
+        super.readAdditionalSaveData(compoundTag);
+        this.setHunting(compoundTag.getBoolean("IsHunting"));
+        this.setHuntingTick(compoundTag.getInt("HuntingTick"));
     }
 
     class TyrannosaurusRoarGoal extends Goal {
@@ -156,7 +225,7 @@ public class TyrannosaurusEntity extends RiftCreature implements IAnimatable {
 
         public TyrannosaurusRoarGoal(TyrannosaurusEntity mob) {
             this.mob = mob;
-            this.roarChance = new Random().nextInt(4);
+            this.roarChance = new Random().nextInt(3);
         }
 
         @Override
