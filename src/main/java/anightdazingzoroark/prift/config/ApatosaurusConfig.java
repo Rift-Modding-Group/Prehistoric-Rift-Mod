d1b862bef7169dc7d67e4d015b51e85941f38b69
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2023-12-18 19:57:51 +0800

    apotatosaurus

--- src/main/java/anightdazingzoroark/prift/config/ApatosaurusConfig.java
+++ src/main/java/anightdazingzoroark/prift/config/ApatosaurusConfig.java
@@ -7,7 +7,7 @@ public class ApatosaurusConfig extends RiftConfig {
     private static int initMaxHealth;
     public static int damage;
     private static int initDamage;
-    public static String[] apatosaurusFavoriteFood = {"minecraft:apple:0:0.025", "minecraft:wheat:0:0.05", "minecraft:carrot:0:0.05", "minecraft:potato:0:0.05", "minecraft:beetroot:0:0.05"};
+    public static String[] apatosaurusFavoriteFood = {"minecraft:leaves:32767:0.025", "minecraft:leaves2:32767:0.025"};
     public static String[] apatosaurusBreedingFood = {"prift:basic_herbivore_meal:0:0", "prift:advanced_herbivore_meal:0:0"};
 
     public ApatosaurusConfig(Configuration config) {
@@ -22,7 +22,7 @@ public class ApatosaurusConfig extends RiftConfig {
         maxHealth = config.getInt("Max health for this creature", "Creature Stats", initMaxHealth, 1, 69420, "Maximum health of this creature");
         damage = config.getInt("Max damage for this creature", "Creature Stats", initDamage, 0, 69420, "Maximum (melee) damage of this creature");
 
-        apatosaurusFavoriteFood = config.getStringList("Apatosaurus Favorite Food", "General", new String[]{"minecraft:apple:0:0.025", "minecraft:wheat:0:0.05", "minecraft:carrot:0:0.05", "minecraft:potato:0:0.05", "minecraft:beetroot:0:0.05"}, "List of foods Apatosauruses will eat (when tamed) or pick up when on the ground. To add items add \"<insert item's identifier here>:<insert data id here>:<insert percentage of health that will be healed upon consumption here>\"");
+        apatosaurusFavoriteFood = config.getStringList("Apatosaurus Favorite Food", "General", new String[]{"minecraft:leaves:32767:0.025", "minecraft:leaves2:32767:0.025"}, "List of foods Apatosauruses will eat (when tamed) or pick up when on the ground. To add items add \"<insert item's identifier here>:<insert data id here>:<insert percentage of health that will be healed upon consumption here>\"");
         apatosaurusBreedingFood = config.getStringList("Apatosaurus Breeding Food", "General", new String[]{"prift:basic_herbivore_meal:0:0", "prift:advanced_herbivore_meal:0:0"}, "List of foods Apatosauruses need to be fed in order to breed. To add items add \"<insert item's identifier here>:<insert data id here>:0\"");
     }
 
--- src/main/java/anightdazingzoroark/prift/server/entity/creature/Apatosaurus.java
+++ src/main/java/anightdazingzoroark/prift/server/entity/creature/Apatosaurus.java
@@ -2,6 +2,7 @@ package anightdazingzoroark.prift.server.entity.creature;
 
 import anightdazingzoroark.prift.config.ApatosaurusConfig;
 import anightdazingzoroark.prift.server.entity.RiftCreatureType;
+import anightdazingzoroark.prift.server.entity.ai.*;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.network.datasync.DataParameter;
@@ -9,6 +10,11 @@ import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 
 public class Apatosaurus extends RiftCreature {
@@ -22,7 +28,9 @@ public class Apatosaurus extends RiftCreature {
         this.favoriteFood = ApatosaurusConfig.apatosaurusFavoriteFood;
         this.tamingFood = ApatosaurusConfig.apatosaurusBreedingFood;
         this.experienceValue = 50;
-        this.speed = 0.10D;
+        this.speed = 0.15D;
+        this.isRideable = true;
+        this.attackWidth = 4.5f;
     }
 
     @Override
@@ -37,6 +45,27 @@ public class Apatosaurus extends RiftCreature {
         this.getAttributeMap().registerAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(ApatosaurusConfig.damage);
         this.getEntityAttribute(SharedMonsterAttributes.KNOCKBACK_RESISTANCE).setBaseValue(1D);
         this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(16D);
+    }
+
+    protected void initEntityAI() {
+        this.targetTasks.addTask(1, new RiftHurtByTarget(this, false));
+        this.tasks.addTask(1, new RiftMate(this));
+        this.tasks.addTask(2, new RiftControlledAttack(this, 3F, 3F));
+        this.tasks.addTask(3, new RiftAttack(this, 1.0D, 3F, 3F));
+        this.tasks.addTask(4, new RiftFollowOwner(this, 1.0D, 10.0F, 2.0F));
+        this.tasks.addTask(5, new RiftMoveToHomePos(this, 1.0D));
+        this.tasks.addTask(6, new RiftWander(this, 1.0D));
+        this.tasks.addTask(7, new RiftLookAround(this));
+    }
+
+    @Override
+    public boolean canBeSaddled() {
+        return true;
+    }
+
+    @Override
+    public int slotCount() {
+        return 54;
     }
 
     @Override
@@ -74,6 +103,30 @@ public class Apatosaurus extends RiftCreature {
 
     @Override
     public void registerControllers(AnimationData data) {
+        data.addAnimationController(new AnimationController(this, "movement", 0, this::apatosaurusMovement));
+        data.addAnimationController(new AnimationController(this, "attack", 0, this::apatosaurusAttack));
+    }
 
+    private <E extends IAnimatable> PlayState apatosaurusMovement(AnimationEvent<E> event) {
+        if (this.isSitting() && !this.isBeingRidden() && !this.hasTarget()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.apatosaurus.sitting", true));
+            return PlayState.CONTINUE;
+        }
+        if (event.isMoving() || (this.isSitting() && this.hasTarget()) && !this.isAttacking()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.apatosaurus.walk", true));
+            return PlayState.CONTINUE;
+        }
+        event.getController().clearAnimationCache();
+        return PlayState.STOP;
+    }
+
+    private <E extends IAnimatable> PlayState apatosaurusAttack(AnimationEvent<E> event) {
+        if (this.isAttacking()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.apatosaurus.stomp", false));
+        }
+        else {
+            event.getController().clearAnimationCache();
+        }
+        return PlayState.CONTINUE;
     }
 }
