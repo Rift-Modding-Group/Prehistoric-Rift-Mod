4cf22e68330f8a61e7ac4d737ab03aa2a88c922f
ANightDazingZoroark <anightdazingzoroark22@gmail.com>
2024-07-27 23:31:58 +0800

    attempt to improve tyranno roar ability

--- src/main/java/anightdazingzoroark/prift/server/entity/ai/RiftTyrannosaurusRoar.java
+++ src/main/java/anightdazingzoroark/prift/server/entity/ai/RiftTyrannosaurusRoar.java
@@ -4,56 +4,75 @@ import anightdazingzoroark.prift.client.RiftSounds;
 import anightdazingzoroark.prift.server.entity.creature.Tyrannosaurus;
 import anightdazingzoroark.prift.server.enums.TameBehaviorType;
 import anightdazingzoroark.prift.server.enums.TameStatusType;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAIBase;
 
 import java.util.Random;
 
 public class RiftTyrannosaurusRoar extends EntityAIBase {
-    protected final Tyrannosaurus mob;
+    private final Tyrannosaurus creature;
     private int roarTick;
-    private int revengeTimerOld;
+    private int failAttemptTimer;
 
     public RiftTyrannosaurusRoar(Tyrannosaurus mob) {
-        this.mob = mob;
-        this.revengeTimerOld = this.mob.getRevengeTimer();
+        this.creature = mob;
         this.roarTick = 0;
+        this.failAttemptTimer = 0;
+        this.setMutexBits(3);
     }
 
     @Override
     public boolean shouldExecute() {
-        if (!this.mob.isTamed()) {
-            return !this.mob.isActing() && this.mob.hurtTime > 0 && new Random().nextInt(4) == 0 && this.mob.canRoar();
+        if (this.failAttemptTimer <= 0) {
+            if (this.creature.hurtTime > 0) {
+                int randVal = new Random().nextInt(2);
+                if (randVal == 0) {
+                    if (!this.creature.isTamed()) {
+                        return !this.creature.isActing() && this.creature.canRoar();
+                    }
+                    else return this.creature.getPassengers().isEmpty() && !this.creature.isActing() && this.creature.canRoar() && this.creature.getTameStatus() != TameStatusType.SIT && this.creature.getTameBehavior() != TameBehaviorType.PASSIVE;
+                }
+                else {
+                    this.failAttemptTimer = 10;
+                    return false;
+                }
+            }
+            else return false;
         }
-        else return this.mob.getPassengers().isEmpty() && !this.mob.isActing() && this.mob.hurtTime > 0 && new Random().nextInt(4) == 0 && this.mob.canRoar() && this.mob.getTameStatus() != TameStatusType.SIT && this.mob.getTameBehavior() != TameBehaviorType.PASSIVE;
+        else {
+            this.failAttemptTimer--;
+            return false;
+        }
     }
 
     @Override
     public boolean shouldContinueExecuting() {
-        return this.roarTick <= 40 && this.mob.isEntityAlive();
+        return this.roarTick <= 40 && this.creature.isEntityAlive();
     }
 
     @Override
     public void startExecuting() {
-        this.mob.setRoaring(true);
-        this.mob.removeSpeed();
+        this.creature.setRoaring(true);
+        this.creature.removeSpeed();
+        this.failAttemptTimer = 0;
     }
 
     @Override
     public void resetTask() {
         this.roarTick = 0;
-        this.mob.setRoaring(false);
-        this.mob.setCanRoar(false);
-        this.mob.resetSpeed();
-        if (this.mob.isTamed()) this.mob.roarCharge = 0;
+        this.creature.setRoaring(false);
+        this.creature.setCanRoar(false);
+        this.creature.resetSpeed();
+        if (this.creature.isTamed()) this.creature.roarCharge = 0;
     }
 
     @Override
     public void updateTask() {
         this.roarTick++;
-        if (this.roarTick == 10 && this.mob.isEntityAlive()) {
-            this.mob.roar(0.015f * this.mob.roarCharge + 1.5f);
-            if (this.mob.isTamed()) this.mob.setEnergy(this.mob.getEnergy() - (int)(0.06d * (double)this.mob.roarCharge + 6d));
-            this.mob.playSound(RiftSounds.TYRANNOSAURUS_ROAR, 2, 1);
+        if (this.roarTick == 10 && this.creature.isEntityAlive()) {
+            this.creature.roar(0.015f * this.creature.roarCharge + 1.5f);
+            if (this.creature.isTamed()) this.creature.setEnergy(this.creature.getEnergy() - (int)(0.06d * (double)this.creature.roarCharge + 6d));
+            this.creature.playSound(RiftSounds.TYRANNOSAURUS_ROAR, 2, 1);
         }
     }
 }
