b8c7f4f5b224b6f6b005b30e014feb28a081012a
ANightDazingZoroark <anightdazingzoroark22@gmail.com>
2025-07-02 10:43:54 +0800

    baryonyx hitboxes

--- src/main/java/anightdazingzoroark/prift/server/entity/creature/Baryonyx.java
+++ src/main/java/anightdazingzoroark/prift/server/entity/creature/Baryonyx.java
@@ -24,16 +24,6 @@ import java.util.Map;
 
 public class Baryonyx extends RiftWaterCreature {
     public static final ResourceLocation LOOT =  LootTableList.register(new ResourceLocation(RiftInitialize.MODID, "entities/baryonyx"));
-    private RiftCreaturePart mainHeadPart;
-    private RiftCreaturePart neckPart;
-    private RiftCreaturePart frontBodyPart;
-    private RiftCreaturePart leftLegPart;
-    private RiftCreaturePart rightLegPart;
-    private RiftCreaturePart tail0Part;
-    private RiftCreaturePart tail1Part;
-    private RiftCreaturePart tail2Part;
-    private RiftCreaturePart tail3Part;
-    private RiftCreaturePart tail4Part;
 
     public Baryonyx(World worldIn) {
         super(worldIn, RiftCreatureType.BARYONYX);
@@ -46,33 +36,6 @@ public class Baryonyx extends RiftWaterCreature {
         this.speed = 0.25D;
         this.waterSpeed = 5D;
         this.targetList = RiftUtil.creatureTargets(RiftConfigHandler.getConfig(this.creatureType).general.targetWhitelist, RiftConfigHandler.getConfig(this.creatureType).general.targetBlacklist, true);
-
-        this.bodyPart = new RiftCreaturePart(this, 0, 0, 1.25f, 0.9f, 1f, 1f);
-        this.headPart = new RiftCreaturePart(this, 4f, 0, 2.125f, 0.75f, 0.5f, 1.5f);
-        this.mainHeadPart = new RiftCreaturePart(this, 3, 0, 2.125f, 0.625f, 0.625f, 1.5f);
-        this.neckPart = new RiftCreaturePart(this, 2.25f, 0, 1.625f, 0.5f, 1f, 1.5f);
-        this.frontBodyPart = new RiftCreaturePart(this, 1.25f, 0, 1.25f, 0.9f, 0.9f, 1f);
-        this.leftLegPart = new RiftCreaturePart(this, 0.75f, -140f, 0, 0.5f, 1.3f, 0.5f);
-        this.rightLegPart = new RiftCreaturePart(this, 0.75f, 140f, 0, 0.5f, 1.3f, 0.5f);
-        this.tail0Part = new RiftCreaturePart(this, -1f, 0, 1.5f, 0.6f, 0.7f, 0.5f);
-        this.tail1Part = new RiftCreaturePart(this, -1.75f, 0, 1.5f, 0.6f, 0.7f, 0.5f);
-        this.tail2Part = new RiftCreaturePart(this, -2.5f, 0, 1.45f, 0.5f, 0.7f, 0.5f);
-        this.tail3Part = new RiftCreaturePart(this, -3.25f, 0, 1.45f, 0.5f, 0.6f, 0.5f);
-        this.tail4Part = new RiftCreaturePart(this, -4f, 0, 1.4f, 0.5f, 0.6f, 0.5f);
-        this.hitboxArray = new RiftCreaturePart[]{
-            this.bodyPart,
-            this.headPart,
-            this.mainHeadPart,
-            this.neckPart,
-            this.frontBodyPart,
-            this.leftLegPart,
-            this.rightLegPart,
-            this.tail0Part,
-            this.tail1Part,
-            this.tail2Part,
-            this.tail3Part,
-            this.tail4Part
-        };
     }
 
     @Override
@@ -102,23 +65,6 @@ public class Baryonyx extends RiftWaterCreature {
         this.tasks.addTask(6, new RiftGoToWater(this, 16, 1.0D));
         this.tasks.addTask(7, new RiftWanderWater(this, 1.0D));
         this.tasks.addTask(8, new RiftWander(this, 1.0D));
-    }
-
-    @Override
-    public void updateParts() {
-        super.updateParts();
-
-        float sitOffset = (this.isSitting() && !this.isBeingRidden() && !this.isInWater()) ? -0.75f : 0;
-        if (this.headPart != null) this.headPart.setPositionAndUpdate(this.headPart.posX, this.headPart.posY + sitOffset, this.headPart.posZ);
-        if (this.bodyPart != null) this.bodyPart.setPositionAndUpdate(this.bodyPart.posX, this.bodyPart.posY + sitOffset, this.bodyPart.posZ);
-        if (this.mainHeadPart != null) this.mainHeadPart.setPositionAndUpdate(this.mainHeadPart.posX, this.mainHeadPart.posY + sitOffset, this.mainHeadPart.posZ);
-        if (this.neckPart != null) this.neckPart.setPositionAndUpdate(this.neckPart.posX, this.neckPart.posY + sitOffset, this.neckPart.posZ);
-        if (this.frontBodyPart != null) this.frontBodyPart.setPositionAndUpdate(this.frontBodyPart.posX, this.frontBodyPart.posY + sitOffset, this.frontBodyPart.posZ);
-        if (this.tail0Part != null) this.tail0Part.setPositionAndUpdate(this.tail0Part.posX, this.tail0Part.posY + sitOffset, this.tail0Part.posZ);
-        if (this.tail1Part != null) this.tail1Part.setPositionAndUpdate(this.tail1Part.posX, this.tail1Part.posY + sitOffset, this.tail1Part.posZ);
-        if (this.tail2Part != null) this.tail2Part.setPositionAndUpdate(this.tail2Part.posX, this.tail2Part.posY + sitOffset, this.tail2Part.posZ);
-        if (this.tail3Part != null) this.tail3Part.setPositionAndUpdate(this.tail3Part.posX, this.tail3Part.posY + sitOffset, this.tail3Part.posZ);
-        if (this.tail4Part != null) this.tail4Part.setPositionAndUpdate(this.tail4Part.posX, this.tail4Part.posY + sitOffset, this.tail4Part.posZ);
     }
 
     @Override
