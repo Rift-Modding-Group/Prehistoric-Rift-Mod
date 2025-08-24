9fee6d1d2f7508713d3d3b7408f80e62a59e4615
ANightDazingZoroark <anightdazingzoroark22@gmail.com>
2025-08-24 19:16:26 +0800

    hmmm

--- src/main/java/anightdazingzoroark/prift/server/tileentities/RiftNewTileEntityCreatureBox.java
+++ src/main/java/anightdazingzoroark/prift/server/tileentities/RiftNewTileEntityCreatureBox.java
@@ -3,21 +3,59 @@ package anightdazingzoroark.prift.server.tileentities;
 import anightdazingzoroark.prift.helper.FixedSizeList;
 import anightdazingzoroark.prift.server.blocks.RiftCreatureBox;
 import anightdazingzoroark.prift.server.capabilities.playerTamedCreatures.CreatureNBT;
+import anightdazingzoroark.prift.server.capabilities.playerTamedCreatures.PlayerTamedCreaturesHelper;
+import anightdazingzoroark.prift.server.entity.creature.RiftCreature;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
+import net.minecraft.util.math.BlockPos;
 
 import javax.annotation.Nullable;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class RiftNewTileEntityCreatureBox extends TileEntity implements ITickable {
     private final FixedSizeList<CreatureNBT> creatureListNBT = new FixedSizeList<>(RiftCreatureBox.maxDeployableCreatures, new CreatureNBT());
 
     @Override
-    public void update() {}
+    public void update() {
+        //if box has contents, make it so that its indestructible when there's creatures inside
+        RiftCreatureBox creatureBox = (RiftCreatureBox)this.world.getBlockState(this.pos).getBlock();
+        if (this.isUnbreakable()) creatureBox.setHardness(-1.0f);
+        else creatureBox.setHardness(0f);
+    }
+
+    //this creates the creatures that wander around the box
+    private void createCreaturesForWandering() {
+        this.world.getChunk(this.pos);
+        for (CreatureNBT tagCompound : this.creatureListNBT.getList()) {
+            RiftCreature creature = tagCompound.getCreatureAsNBT(this.world);
+
+            //check for validity and if creature already exists, then continue
+            if (creature == null || this.creatureExistsInWorld(creature) || !creature.isEntityAlive()) continue;
+
+            //generate a spawn point for creature
+            BlockPos spawnPoint = RiftTileEntityCreatureBoxHelper.creatureCreatureSpawnPoint(this.pos, this.world, creature);
+            if (spawnPoint != null) {
+                creature.setPosition(spawnPoint.getX(), spawnPoint.getY(), spawnPoint.getZ());
+                creature.setHomePos(this.pos.getX(), this.pos.getY(), this.pos.getZ());
+                this.world.spawnEntity(creature);
+            }
+        }
+    }
+
+    private boolean creatureExistsInWorld(RiftCreature creature) {
+        List<UUID> worldEntityListUUID = this.world.getLoadedEntityList().stream()
+                .map(Entity::getUniqueID)
+                .collect(Collectors.toList());
+        return worldEntityListUUID.contains(creature.getUniqueID());
+    }
 
     public boolean isUnbreakable() {
         return !this.creatureListNBT.isEmpty();
