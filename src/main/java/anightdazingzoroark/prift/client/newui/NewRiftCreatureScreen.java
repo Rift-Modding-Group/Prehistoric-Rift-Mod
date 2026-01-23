632b98fb74a82d7160a47d18cd83d1f887a26723
ANightDazingZoroark <anightdazingzoroark22@gmail.com>
2026-01-24 04:31:00 +0800

    more changes

--- src/main/java/anightdazingzoroark/prift/client/newui/NewRiftCreatureScreen.java
+++ src/main/java/anightdazingzoroark/prift/client/newui/NewRiftCreatureScreen.java
@@ -1,10 +1,14 @@
 package anightdazingzoroark.prift.client.newui;
 
+import anightdazingzoroark.prift.server.capabilities.playerTamedCreatures.PlayerTamedCreatures;
 import anightdazingzoroark.prift.server.entity.creature.RiftCreature;
+import anightdazingzoroark.prift.server.entity.interfaces.IHarvestWhenWandering;
+import anightdazingzoroark.prift.server.entity.interfaces.IWorkstationUser;
 import anightdazingzoroark.prift.server.enums.TameBehaviorType;
 import com.cleanroommc.modularui.api.drawable.IKey;
 import com.cleanroommc.modularui.api.widget.IWidget;
 import com.cleanroommc.modularui.drawable.GuiTextures;
+import com.cleanroommc.modularui.drawable.ItemDrawable;
 import com.cleanroommc.modularui.factory.EntityGuiData;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.screen.UISettings;
@@ -22,6 +26,7 @@ import com.cleanroommc.modularui.widgets.layout.Row;
 import com.cleanroommc.modularui.widgets.slot.ItemSlot;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.init.Blocks;
 import net.minecraftforge.items.ItemStackHandler;
 
 public class NewRiftCreatureScreen {
@@ -36,16 +41,13 @@ public class NewRiftCreatureScreen {
                 .coverChildren()
                 .topRel(0f, 4, 1f)
                 .child(new PageButton(0, tabController)
-                        .overlay(IKey.str("Inventory"))
+                        .overlay(new ItemDrawable(Blocks.CHEST).asIcon())
                         .tab(GuiTextures.TAB_TOP, -1))
                 .child(new PageButton(1, tabController)
-                        .overlay(IKey.str("Settings"))
-                        .tab(GuiTextures.TAB_TOP, 0))
-                .child(new PageButton(2, tabController)
-                        .overlay(IKey.str("Information"))
+                        .overlay(GuiTextures.GEAR.asIcon().size(24))
                         .tab(GuiTextures.TAB_TOP, 0));
         PagedWidget<?> pagedWidget = new PagedWidget<>()
-                .sizeRel(1f)
+                .debugName("pagedWidget").sizeRel(1f)
                 .controller(tabController)
                 .addPage(creatureInventoryPage(creature, syncManager))
                 .addPage(creatureSettingsPage(creature, syncManager))
@@ -54,6 +56,7 @@ public class NewRiftCreatureScreen {
         return new ModularPanel("creatureScreen").size(180, 192)
                 .child(tabButtonRow)
                 .child(pagedWidget);
+                //.coverChildrenHeight(); this is here until i figure out how to dynamically change height of a ModularPanel
     }
 
     private static ParentWidget<?> creatureInventoryPage(RiftCreature creature, PanelSyncManager syncManager) {
@@ -129,6 +132,11 @@ public class NewRiftCreatureScreen {
         );
         syncManager.syncValue("creatureSitting", sittingValue);
 
+        //creature information button
+        ButtonWidget<?> creatureInfoButton = new ButtonWidget<>()
+                .overlay(GuiTextures.EXCLAMATION)
+                .size(12).align(Alignment.TopRight);
+
         //set sitting
         Flow sittingOptions = new Row()
                 .coverChildrenHeight()
@@ -144,30 +152,38 @@ public class NewRiftCreatureScreen {
                 )
                 .child(IKey.str("Sitting").asWidget().verticalCenter());
 
+        //define top parentwidget
+        ParentWidget<?> topParentWidget = new ParentWidget<>()
+                .debugName("top")
+                .widthRel(1f)
+                .coverChildrenHeight()
+                .align(Alignment.TopCenter)
+                .child(sittingOptions);
+
+        //define bottom parentwidget
+        boolean hasOptionsButtons = canHaveOptionsButtons(creature);
+        ParentWidget<?> bottomParentWidget = new ParentWidget<>()
+                .debugName("bottom")
+                .widthRel(1f)
+                .coverChildrenHeight()
+                .align(Alignment.BottomCenter);
+        if (hasOptionsButtons) {
+            bottomParentWidget.child(creatureBehaviorGroup(creature, syncManager, true).align(Alignment.TopLeft))
+                    .child(creatureOptionsGroup(creature, syncManager).align(Alignment.TopRight));
+        }
+        else bottomParentWidget.child(creatureBehaviorGroup(creature, syncManager, false).align(Alignment.TopCenter));
+
         //final return value
         return new ParentWidget<>().sizeRel(1f, 1f)
                 .padding(7, 7)
-                .child(new ParentWidget<>()
-                        .debugName("top")
-                        .widthRel(1f)
-                        .coverChildrenHeight()
-                        .align(Alignment.TopCenter)
-                        .child(sittingOptions)
-                )
-                .child(new ParentWidget<>()
-                        .debugName("bottom")
-                        .widthRel(1f)
-                        .coverChildrenHeight()
-                        .align(Alignment.BottomCenter)
-                        .child(new Row()
-                                .coverChildren()
-                                .child(creatureBehaviorGroup(creature, syncManager))
-                                .child(creatureOptionsGroup(creature, syncManager))
-                        )
-                );
+                .child(creatureInfoButton)
+                .child(topParentWidget)
+                .child(bottomParentWidget);
     }
 
-    private static ParentWidget<?> creatureBehaviorGroup(RiftCreature creature, PanelSyncManager syncManager) {
+    private static ParentWidget<?> creatureBehaviorGroup(RiftCreature creature, PanelSyncManager syncManager, boolean hasOptionsButtons) {
+        int buttonWidth = hasOptionsButtons ? 80 : 160;
+
         //creature tame behavior syncing
         IntSyncValue creatureBehaviorValue = new IntSyncValue(
                 () -> creature.getTameBehavior().ordinal(),
@@ -176,7 +192,7 @@ public class NewRiftCreatureScreen {
         syncManager.syncValue("creatureBehavior", creatureBehaviorValue);
 
         Flow behaviorOptions = new Column()
-                .coverChildren()
+                .coverChildrenHeight().width(buttonWidth)
                 .childPadding(3)
                 .child(IKey.lang("radial.choice.behavior").asWidget());
 
@@ -186,7 +202,7 @@ public class NewRiftCreatureScreen {
             if (!creature.canEnterTurretMode() && behavior == TameBehaviorType.TURRET) continue;
 
             behaviorOptions.child(
-                    new ToggleButton().size(66, 20)
+                    new ToggleButton().size(buttonWidth, 20)
                             .overlay(IKey.lang(behavior.getTranslatedName()))
                             .value(new BoolValue.Dynamic(
                                     () -> creatureBehaviorValue.getIntValue() == behavior.ordinal() && creature.getTameBehavior() == behavior,
@@ -198,11 +214,32 @@ public class NewRiftCreatureScreen {
         return behaviorOptions;
     }
 
+    private static boolean canHaveOptionsButtons(RiftCreature creature) {
+        return creature.getDeploymentType() == PlayerTamedCreatures.DeploymentType.BASE &&
+                (creature instanceof IWorkstationUser || creature instanceof IHarvestWhenWandering);
+    }
+
     private static ParentWidget<?> creatureOptionsGroup(RiftCreature creature, PanelSyncManager syncManager) {
         Flow creatureOptions = new Column()
-                .coverChildrenHeight().width(66)
+                .coverChildrenHeight().width(80)
                 .childPadding(3)
                 .child(IKey.lang("radial.choice.options").asWidget());
+
+        //workstation button
+        if (creature instanceof IWorkstationUser) {
+            creatureOptions.child(
+                    new ButtonWidget<>().size(80, 20)
+                            .overlay(IKey.lang("radial.choice.set_workstation"))
+            );
+        }
+
+        //harvest on wander button
+        if (creature instanceof IHarvestWhenWandering) {
+            creatureOptions.child(
+                    new ButtonWidget<>().size(80, 20)
+                            .overlay(IKey.lang("radial.choice.set_wander_harvest"))
+            );
+        }
 
         return creatureOptions;
     }
---
+++ src/main/java/anightdazingzoroark/prift/client/newui/RiftCreatureInfoScreen.java
@@ -0,0 +1,6 @@
+package anightdazingzoroark.prift.client.newui;
+
+//this one class will be used to display information about a creature
+//in the form of a pop-up, and will be shared amongst various uis
+public class RiftCreatureInfoScreen {
+}
